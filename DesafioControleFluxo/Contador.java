b537f763bcf40aa12b4673811c2ebfd52594b7e3
kaweny <kawenyv@gmail.com>
2023-10-10 00:14:19 -0300

    DesafioControleFluxo

---
+++ DesafioControleFluxo/Contador.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException exception) {
+            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+
+        int contagem = parametroDois - parametroUm;
+        if (contagem < 0){
+            throw new ParametrosInvalidosException();
+        }
+
+        else{
+            for (int x = parametroUm + 1; x < parametroDois; x++){
+                System.out.println("Imprimindo o número " + x);
+            }
+        }
+    }
+}
+
